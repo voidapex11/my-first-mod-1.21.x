35f0a7a445f13358ca16b2b64e50c906e010f162
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-09-29 16:44:14 +0100

    initial commit

---
+++ src/main/java/com/voidapex11/myfirstmod/Myfirstmod.java
@@ -0,0 +1,16 @@
+package com.voidapex11.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Myfirstmod implements ModInitializer {
+	public static final String MOD_ID = "my-first-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/voidapex11/myfirstmod/myfirstmodClientClass.java
@@ -0,0 +1,11 @@
+package com.voidapex11.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class myfirstmodClientClass implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
